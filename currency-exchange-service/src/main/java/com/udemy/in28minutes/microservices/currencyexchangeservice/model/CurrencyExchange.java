83fb0c13279b23b2c359fba8eae3aae920b5df74
bhanushalij <bhanushalij@PMCLAP1607-1222.pmc.local>
2023-06-22 17:11:12 +0530

    Added jpa and h2 dependencies and created data.sql to load some data in currency_exchange table

--- currency-exchange-service/src/main/java/com/udemy/in28minutes/microservices/currencyexchangeservice/model/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/udemy/in28minutes/microservices/currencyexchangeservice/model/CurrencyExchange.java
@@ -2,9 +2,20 @@ package com.udemy.in28minutes.microservices.currencyexchangeservice.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class CurrencyExchange {
+	
+	@Id
 	private long id;
+	
+	@Column(name="currency_from")
 	private String from;
+	
+	@Column(name="currency_to")
 	private String to;
 	private BigDecimal conversionMultiple;
 	private String environment;
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert INTO currency_exchange(id,currency_from,currency_to,conversion_multiple,environment) values(10002,'USD','INR',65,'');
+insert INTO currency_exchange(id,currency_from,currency_to,conversion_multiple,environment) values(10003,'EUR','INR',75,'');
+insert INTO currency_exchange(id,currency_from,currency_to,conversion_multiple,environment) values(10004,'AUD','INR',25,'');
