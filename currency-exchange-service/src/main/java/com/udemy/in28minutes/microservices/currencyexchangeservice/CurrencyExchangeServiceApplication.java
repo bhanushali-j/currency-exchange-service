95ab6d32a582eb20c5f1aa5831780c61ec509ee4
bhanushalij <bhanushalij@PMCLAP1607-1222.pmc.local>
2023-06-23 17:59:13 +0530

    Added eureka client and registered the service with eureka

--- currency-exchange-service/src/main/java/com/udemy/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/udemy/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,6 +2,8 @@ package com.udemy.in28minutes.microservices.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 public class CurrencyExchangeServiceApplication {
