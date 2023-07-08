25b41c84805d62b470fc44cbe6c39306a05d44d2
bhanushalij <bhanushalij@PMCLAP1607-1222.pmc.local>
2023-07-08 13:01:04 +0530

    Added dependencies for zipkin

--- currency-exchange-service/src/main/java/com/udemy/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/udemy/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.udemy.in28minutes.microservices.currencyexchangeservice.controller;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,8 @@ import com.udemy.in28minutes.microservices.currencyexchangeservice.repository.Cu
 @RestController
 public class CurrencyExchangeController {
 	
+	private Logger logger=LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	@Autowired
 	CurrencyExchangeRepository currencyExchangeRepository;
 	
@@ -24,7 +28,7 @@ public class CurrencyExchangeController {
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 //		CurrencyExchange currencyExchange = new CurrencyExchange(1,from,to,BigDecimal.valueOf(50));
 		
-		
+		logger.info("retrieveExchangeValue called with {} to {}",from,to);
 		CurrencyExchange currencyExchange=currencyExchangeRepository.findByFromAndTo(from, to);
 		if(currencyExchange==null) {
 			throw new RuntimeException("Currency not found for : "+from+" to : "+to);
